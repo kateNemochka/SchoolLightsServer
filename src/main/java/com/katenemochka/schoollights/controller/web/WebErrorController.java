70995efb51a959f32f174fddff7335189fbcb93b
Kateryna Nemchenko <nemochka99@gmail.com>
2021-06-08 01:48:58 +0300

    Error pages views and controllers improvement: footer is in separate file, css and js links are replaced with thymeleaf replace tag

--- src/main/java/com/katenemochka/schoollights/controller/web/WebErrorController.java
+++ src/main/java/com/katenemochka/schoollights/controller/web/WebErrorController.java
@@ -3,6 +3,7 @@ package com.katenemochka.schoollights.controller.web;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -12,11 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 public class WebErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
         if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
+            int statusCode = Integer.parseInt(status.toString());
+            model.addAttribute("error_code", RequestDispatcher.ERROR_STATUS_CODE);
+            model.addAttribute("error_message", RequestDispatcher.ERROR_MESSAGE);
+            model.addAttribute("error_exception", RequestDispatcher.ERROR_EXCEPTION);
+            model.addAttribute("error_uri", RequestDispatcher.ERROR_REQUEST_URI);
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error-404";
@@ -24,6 +28,7 @@ public class WebErrorController implements ErrorController {
             else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
                 return "error-500";
             }
+            else return "error";
         }
         return "error";
     }
